97f343b0bed23a1310ca252a1fcbec25b3d44c8c
Mike Nanto <mike@thecowboymechanic.com>
2021-01-04 04:38:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line(katz_deli)
   when 0 
     puts "The line is currently empty."
   else 
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     katz_deli.each.with_index(1) do |name, spot|
     current_line << " #{spot}. #{name}"
     end
